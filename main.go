48af0c56cd78526b21d6c5232d9577260fc89f7c
KIM BUMNYEONG <kimjoin2@gmail.com>
2019-05-01 16:40:15 +0900

    Fix circleci and http web server

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 func main() {
-	print("Hello World!")
+	http.HandleFunc("/", TestController)
+	log.Println("server start!")
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func TestController(w http.ResponseWriter, r *http.Request) {
+	_ = r
+	_, err := fmt.Fprintln(w, "welcome!")
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 }
