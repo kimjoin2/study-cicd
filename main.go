e979c123a98f4c70522be49757b3453a4e3d47ff
bumnyeong kim <bumnyeong@fairway-corp.co.jp>
2019-06-12 11:31:34 +0900

    fix string

--- main.go
+++ main.go
@@ -16,10 +16,7 @@ func main() {
 }
 
 func TestController(w http.ResponseWriter, r *http.Request) {
-	html := `
-<video controls controls loop>
-    <source src="https://d2qguwbxlx1sbt.cloudfront.net/TextInMotion-VideoSample-1080p.mp4" type="video/mp4">
-</video>`
+	html := "<video controls loop><source src'https://d2qguwbxlx1sbt.cloudfront.net/TextInMotion-VideoSample-1080p.mp4' type='video/mp4'></video>"
 	_ = r
 	_, err := fmt.Fprintln(w, html)
 	if err != nil {
