dd4c5f65d98607bb46f40c6f74eab90abdfa3f2b
bumnyeong kim <bumnyeong@fairway-corp.co.jp>
2019-06-12 11:28:09 +0900

    set play video

--- main.go
+++ main.go
@@ -16,8 +16,12 @@ func main() {
 }
 
 func TestController(w http.ResponseWriter, r *http.Request) {
+	html := `
+<video controls controls loop>
+    <source src="https://d2qguwbxlx1sbt.cloudfront.net/TextInMotion-VideoSample-1080p.mp4" type="video/mp4">
+</video>`
 	_ = r
-	_, err := fmt.Fprintln(w, "<h1>Hello World!</h1>")
+	_, err := fmt.Fprintln(w, html)
 	if err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
