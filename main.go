fa240ebcad449ecc7ac8c00b5aec09cc0bb400dd
bumnyeong kim <bumnyeong@fairway-corp.co.jp>
2019-06-12 11:34:30 +0900

    fix

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 }
 
 func TestController(w http.ResponseWriter, r *http.Request) {
-	html := "<video controls loop><source src'https://d2qguwbxlx1sbt.cloudfront.net/TextInMotion-VideoSample-1080p.mp4' type='video/mp4'></video>"
+	html := "<h5><div><video controls loop><source src'https://d2qguwbxlx1sbt.cloudfront.net/TextInMotion-VideoSample-1080p.mp4' type='video/mp4'></video></div></h5>"
 	_ = r
 	_, err := fmt.Fprintln(w, html)
 	if err != nil {
