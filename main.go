03ba7cab0f7507ac95caa9eaa136d1386b3de149
bumnyeong kim <kim_bn@stoneb.jp>
2019-09-06 18:27:25 +0900

    Fix for letsencrypt

--- main.go
+++ main.go
@@ -2,28 +2,41 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"time"
 )
 
 const BumsKimCom = "bumskim.com"
 
 func main() {
 
+	r := mux.NewRouter()
+	r.PathPrefix("/.well-known/acme-challenge/").Handler(
+		http.StripPrefix("/.well-known/acme-challenge/", http.FileServer(http.Dir("./letsencrypt/"))))
+	http.Handle("/", r)
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "0.0.0.0:80",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Println(srv.ListenAndServe())
 	// TODO : need ssl certification for https
 	//        it has bug (https -> http redirect fail cause of https certification
-	go func() {
-		if err := http.ListenAndServe(":443", http.HandlerFunc(redirectTLS)); err != nil {
-			log.Fatalf("ListenAndServe error: %v", err)
-		}
-	}()
+	//go func() {
+	//	if err := http.ListenAndServe(":443", http.HandlerFunc(redirectTLS)); err != nil {
+	//		log.Fatalf("ListenAndServe error: %v", err)
+	//	}
+	//}()
 
-	log.Println("server start!")
-	http.HandleFunc("/", TestController)
-	err := http.ListenAndServe(":80", nil)
-	if err != nil {
-		log.Println(err)
-	}
+	//log.Println("server start!")
+	////http.HandleFunc("/", TestController)
+	//err := http.ListenAndServe(":80", nil)
+	//if err != nil {
+	//	log.Println(err)
+	//}
 }
 
 func redirectTLS(w http.ResponseWriter, r *http.Request) {
